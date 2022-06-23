b61f5dbbb4fad01c4d90e68c5947d1b261630f56
SmokyPlay <smokyplay.kr@gmail.com>
2022-06-23 21:34:13 +0300

    feat(registration): role Unreg is no longer removed

--- src/commands/RegisterCommand.ts
+++ src/commands/RegisterCommand.ts
@@ -33,7 +33,7 @@ export default class RegisterCommand extends AbstractCommand implements Discord.
         balance: 0
       })
       await member.setNickname(account.name + ' ' + account.tag).catch(() => {});
-      await member.roles.remove("782544002544959518");
+      //await member.roles.remove("782544002544959518");
       setTimeout(() => member.roles.add("781200003586457610"), 3000);
       return {reply: {content: "Аккаунт зарегистрирован!"}}
     }
