7b25f314dd6495fb9c99e43133863e1d299bbc5d
SmokyPlay <smokyplay.kr@gmail.com>
2022-08-10 13:29:37 +0300

    feat(subscriptions): added DM message when user subscription ends

--- src/jobs/SubscriptionsJob.ts
+++ src/jobs/SubscriptionsJob.ts
@@ -1,6 +1,7 @@
 import AbstractJob from "@/abstractions/AbstractJob";
 import JobConfig from "@/types/JobConfig";
 import Subscription from "../types/database/Subscription";
+import {MessageEmbed} from "discord.js";
 
 export default class SubscriptionsJob extends AbstractJob implements JobConfig {
     public interval = 3600000
@@ -14,6 +15,12 @@ export default class SubscriptionsJob extends AbstractJob implements JobConfig {
             if(member) {
                 await member.roles.add("782544002544959518")
                 await member.roles.add("986961407222358076");
+                let embed = new MessageEmbed()
+                    .setColor('#007ef8')
+                    .setAuthor({name: "Подписка закончилась", iconURL: member.user.avatarURL({dynamic: true})})
+                    .setDescription("Ваша подписка на CIS FaceIT закончилась\n" +
+                    "Чтобы продлить ее, перейдите в канал <#987309233391415316> и выберите нужную подписку")
+                await member.send({embeds: [embed]});
             }
         }
     }
