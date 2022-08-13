edde067aa34de6936c6af6aafaaddbf7feb94f1c
Smoky <smokyplay.kr@gmail.com>
2022-08-14 02:03:13 +0300

    feat(report): updated command & interface name

--- src/commands/ReportCommand.ts
+++ src/commands/ReportCommand.ts
@@ -11,19 +11,19 @@ import User from "@/types/database/User";
 import Subscription from "@/types/database/Subscription";
 
 export default class ReportCommand extends AbstractCommand implements ChatInputApplicationCommandData {
-    public name = 'жалоба'
-    public description = "Отправляет жалобу на участника"
+    public name = 'репорт'
+    public description = "Отправляет репорт на участника"
     public options: Array<ApplicationCommandOptionData> = [
         {
             type: "USER",
             name: "участник",
-            description: "Участник, на которого нужно пожаловаться",
+            description: "Участник, на которого нужно отправить репорт",
             required: true
         },
         {
             type: "STRING",
             name: "причина",
-            description: "Причина жалобы",
+            description: "Причина репорта",
             required: true
         }
     ]
@@ -34,12 +34,12 @@ export default class ReportCommand extends AbstractCommand implements ChatInputA
         let reason = interaction.options.getString("причина") as string;
         let embed = new MessageEmbed()
             .setColor('#ff0909')
-            .setAuthor({name: `Жалоба на участника ${reported.user.tag}`, iconURL: reported.displayAvatarURL({dynamic: true})})
+            .setAuthor({name: `Репорт на участника ${reported.user.tag}`, iconURL: reported.displayAvatarURL({dynamic: true})})
             .setDescription(`**Причина:** ${reason}`)
             .addField("Участник", reported.toString(), true)
-            .addField("Жалобу отправил", member.toString(), true)
+            .addField("Репорт отправил", member.toString(), true)
         let channel = interaction.guild.channels.cache.get("790843189552087050") as TextChannel;
-        await channel.send({content: `${member.toString()}, ${reported.toString()}`, embeds: [embed]}).then(msg => msg.startThread({name: "Жалоба"}));
-        return {reply: {content: `Жалоба на участника **${reported.user.tag}** отправлена`}}
+        await channel.send({content: `${member.toString()}, ${reported.toString()}`, embeds: [embed]}).then(msg => msg.startThread({name: "Репорт"}));
+        return {reply: {content: `Репорт на участника **${reported.user.tag}** отправлен`}}
     }
 }
