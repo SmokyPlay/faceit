d864715c8269959b84e8c2b0110dedb788f3cc9f
Smoky <smokyplay.kr@gmail.com>
2022-09-21 19:03:40 +0300

    feat: added logs

--- src/interactions/temporary/PlayersInteraction.ts
+++ src/interactions/temporary/PlayersInteraction.ts
@@ -22,6 +22,8 @@ export default class PlayersInteraction extends AbstractInteraction implements I
     private async player(interaction: SelectMenuInteraction): Promise<InteractionExecutionResultConfig> {
         let value = interaction.values[0];
         let player = this.data.members.find(m => m.discord.id === value);
+        console.log(this.data.lobby)
+        console.log(this.data.members.length)
         let team: 'team1' | 'team2' = this.data.members.length === (this.data.lobby.two ? 4 : 6) ? 'team1' : 'team2';
         this.data[team].push(player);
         this.data.members.splice(this.data.members.indexOf(player), 1);
