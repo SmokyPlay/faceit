426c44b923ace788bd6800a52e6ceb64bcc9941a
Smoky <smokyplay.kr@gmail.com>
2022-09-24 22:15:30 +0300

    feat: added 2 vs 2 lobby without picks

--- src/commands/StartCommand.ts
+++ src/commands/StartCommand.ts
@@ -1,7 +1,7 @@
 import {
     ApplicationCommandOptionData,
     ChatInputApplicationCommandData,
-    CommandInteraction, GuildMember, MessageActionRow, MessageButton, MessageEmbed
+    CommandInteraction, GuildMember, MessageActionRow, MessageButton, MessageEmbed, MessageSelectMenu, VoiceChannel
 } from "discord.js";
 import AbstractCommand from "@/abstractions/AbstractCommand";
 import CommandExecutionResultConfig from "@/types/CommandExecutionResultConfig";
@@ -10,6 +10,13 @@ import StartInteraction from "@/interactions/temporary/StartInteraction";
 import LobbyConfig from "../types/LobbyConfig";
 import properties from '@/properties.json';
 import User from "@/types/database/User";
+import GameMemberConfig from "@/types/GameMemberConfig";
+import ModesInteraction from "@/interactions/temporary/ModesInteraction";
+
+interface CreateTeamsResult {
+    team1: Array<GameMemberConfig>
+    team2: Array<GameMemberConfig>
+}
 
 export default class StartCommand extends AbstractCommand implements ChatInputApplicationCommandData {
     public name = 'старт'
@@ -22,6 +29,40 @@ export default class StartCommand extends AbstractCommand implements ChatInputAp
         if(!lobby) {
             return {reply: {embeds:
                         [CommandError.other(member, "Эту команду можно использовать только находясь в лобби")]}}
+        }
+        let team1 = interaction.guild.channels.cache.get(lobby.team1) as VoiceChannel;
+        let team2 = interaction.guild.channels.cache.get(lobby.team2) as VoiceChannel;
+        if(lobby.two && team1.members.size === 2 && team2.members.size === 2) {
+            let teams = await this.createTeams(team1.members.map(m => m), team2.members.map(m => m));
+            let embed = new MessageEmbed()
+                .setThumbnail("https://media.discordapp.net/attachments/992896807199834153/992898514600349776/unknown.png")
+                .setDescription(`Выберите режим, на котором хотите играть\nВыбирает: ${teams.team1.find(m => m.captain).discord.toString()}`)
+                .addField("Команда 1",
+                    teams.team1.map(memb => memb.discord.toString() + (memb.captain ? '⭐' : ''))
+                        .join("\n") + "\nСчёт: 0", true)
+                .addField("Команда 2",
+                    teams.team2.map(memb => memb.discord.toString() + (memb.captain ? '⭐' : ''))
+                        .join("\n") + "\nСчёт: 0", true)
+            let menu = new MessageSelectMenu()
+                .setCustomId(`${interaction.id}-mode`)
+                .setPlaceholder(`Выберите режим`)
+                .addOptions(properties.modes)
+            let row = new MessageActionRow()
+                .addComponents(menu)
+            let inter = new ModesInteraction({
+                id: interaction.id,
+                allowedUsers: [teams.team1.find(m => m.captain).discord.id],
+                data: {
+                    team1: teams.team1,
+                    team2: teams.team2,
+                    lobby: lobby,
+                    modes: JSON.parse(JSON.stringify(properties.modes)),
+                    selected: [],
+                    oneSelected: false
+                },
+                reply: {embed, row}
+            })
+            return {reply: {embeds: [embed], components: [row]}, interaction: inter}
         }
         let playersCount = lobby.two ? 4 : 6;
         if(member.voice.channel.members.size < playersCount) {
@@ -63,5 +104,30 @@ export default class StartCommand extends AbstractCommand implements ChatInputAp
             reply: {embeds: [embed], components: [row]},
             interaction: inter
         }
+    }
+
+    private async createTeams(team1: Array<GuildMember>, team2: Array<GuildMember>): Promise<CreateTeamsResult> {
+        let result: CreateTeamsResult = {
+            team1: [],
+            team2: []
+        }
+        let captains = [Math.random(), Math.random()]
+        for(let member of team1) {
+            let brawl = await global.mongo.findOne<User>('users', {id: member.id})
+            result.team1.push({
+                discord: member,
+                brawl,
+                captain: captains[0] >= 0.5
+            })
+        }
+        for(let member of team2) {
+            let brawl = await global.mongo.findOne<User>('users', {id: member.id})
+            result.team2.push({
+                discord: member,
+                brawl,
+                captain: captains[0] >= 0.5
+            })
+        }
+        return result;
     }
 }
